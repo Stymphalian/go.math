ec5b04004a4bffe1dfd3b5931a29bf6f5eccdb2b
Stymphalian <saturnslight@gmail.com>
2015-05-08 02:52:45 -0700

    Added todo list

--- lmath/doc.go
+++ lmath/doc.go
@@ -1,12 +1,21 @@
 // lmath is a small 3D linear algebra library which provides support for
-// Vec3, Mat4 and Quaternions. Also supports rotations using euler angles,
-// axis-angle, rotation matrix, and quaternions as well as conversion between them
+// Vec3, Mat4 and Quaternions. Supports rotations using euler angle,axis-angle
+// rotation matrix, quaternions + conversions between representations.
 package lmath
 
 // Conventions
 // Right-Hand coordinate system
 // All angles are specified in Radians
-// Rotations are applied in Yaw => Pitch => Roll order
+// Rotations are applied in Pitch => Yaw => Roll order
 var (
 	Version = 0.0
 )
+
+// TODO:
+// Create Vec2 struct
+// Create Vec4 struct
+// Create Mat3 struct
+// Create Mat2 struct
+// Test glmath functions
+// Touch-up all the documentation
+// Add a license (MIT)
